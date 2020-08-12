32311788c3c859ae0456705c46823b602c4f1d4c
jimmy232 <jamescurriehv@gmail.com>
2020-08-12 12:35:11 +1000

    Update update-NodeRed.sh

--- update-NodeRed.sh
+++ update-NodeRed.sh
@@ -3,9 +3,9 @@
 
 sudo git clone https://github.com/jimmy232/SESS_BS_ID001.git /home/pi/MONICON/SESS_BS_ID001
 
-sudo cp /home/pi/MONICON/SESS_BS_ID001/flow.json .node-red/flows_SESS.json
-sudo cp /home/pi/MONICON/SESS_BS_ID001/flow_cred.json .node-red/flows_SESS_cred.json
-sudo cp /home/pi//MONICON/SESS_BS_ID001/package.json .node-red/package.json
+sudo cp /home/pi/MONICON/SESS_BS_ID001/flow.json /home/pi/.node-red/flows_SESS.json
+sudo cp /home/pi/MONICON/SESS_BS_ID001/flow_cred.json /home/pi/.node-red/flows_SESS_cred.json
+sudo cp /home/pi//MONICON/SESS_BS_ID001/package.json /home/pi/.node-red/package.json
 
 sudo rm -r /home/pi/MONICON/SESS_BS_ID001
 
