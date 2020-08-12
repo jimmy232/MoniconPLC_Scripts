3714afc1b9815a01c8865299c842a0783e026796
jimmy232 <jamescurriehv@gmail.com>
2020-08-12 11:50:35 +1000

    Update and rename update-NodeRed.sh.url to update-NodeRed.sh

---
+++ update-NodeRed.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Download SESS_BS_ID001 from github repository
+
+sudo git clone https://github.com/jimmy232/SESS_BS_ID001.git /home/pi/SESS_BS_ID001
+
+sudo cp /home/pi/MONICON/SESS_BS_ID001/flow.json .node-red/flows_SESS.json
+sudo cp /home/pi/MONICON/SESS_BS_ID001/flow_cred.json .node-red/flows_SESS_cred.json
+sudo cp /home/pi//MONICON/SESS_BS_ID001/package.json .node-red/package.json
+
+sudo rm -r /home/pi/MONICON/SESS_BS_ID001
+
+echo Download complete
+
+sudo reboot
+
+// Enable Bash file
+sudo chmod +x update-SESS_BS_ID001.sh
