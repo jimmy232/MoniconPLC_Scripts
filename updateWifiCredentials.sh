6ee6ac27bc30ad9713282fab351307343a51e887
jimmy232 <jamescurriehv@gmail.com>
2020-09-25 23:24:26 +1000

    Update updateWifiCredentials.sh

--- updateWifiCredentials.sh
+++ updateWifiCredentials.sh
@@ -7,7 +7,7 @@ cd /etc/wpa_supplicant/
 sudo sh -c "echo ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev >> wpa_supplicant.conf"
 sudo sh -c "echo update_config=1 >> wpa_supplicant.conf"
 sudo sh -c "echo country=AU'\n' >> wpa_supplicant.conf"
-sudo sh -c "echo network={ '\n\t'ssid='\"'DeathStar'\"\n\t'psk='\"'jediMasters'\"\n'} >> text.txt"
+sudo sh -c "echo network={ '\n\t'ssid='\"'DeathStar'\"\n\t'psk='\"'jediMasters'\"\n'} >> wpa_supplicant.conf"
 
 echo Download complete
 
