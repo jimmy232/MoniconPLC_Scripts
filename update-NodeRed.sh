712b476cf4493cfafe99db0dd04e5c9d7ea7f08a
jimmy232 <jamescurriehv@gmail.com>
2020-10-30 18:35:47 +1000

    Update update-NodeRed.sh

--- update-NodeRed.sh
+++ update-NodeRed.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-# Download SESS_BS_ID001 from github repository
+# Download SESS_BS_ID002 from github repository
 
-sudo git clone https://github.com/jimmy232/SESS_BS_ID001.git /home/pi/MONICON/SESS_BS_ID001
+sudo git clone https://github.com/jimmy232/SESS_BS_ID002.git /home/pi/MONICON/SESS_BS_ID002
 
-sudo cp /home/pi/MONICON/SESS_BS_ID001/flow.json /home/pi/.node-red/flows_SESS.json
-sudo cp /home/pi/MONICON/SESS_BS_ID001/flow_cred.json /home/pi/.node-red/flows_SESS_cred.json
-sudo cp /home/pi//MONICON/SESS_BS_ID001/package.json /home/pi/.node-red/package.json
+sudo cp /home/pi/MONICON/SESS_BS_ID002/flow.json /home/pi/.node-red/flows_SESS.json
+sudo cp /home/pi/MONICON/SESS_BS_ID002/flow_cred.json /home/pi/.node-red/flows_SESS_cred.json
+sudo cp /home/pi//MONICON/SESS_BS_ID002/package.json /home/pi/.node-red/package.json
 
-//sudo rm -r /home/pi/MONICON/SESS_BS_ID001
+//sudo rm -r /home/pi/MONICON/SESS_BS_ID002
 
 echo Download complete
 
