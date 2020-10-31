096f8d5781dd00262d77d490d9e17e87419b4d64
jimmy232 <jamescurriehv@gmail.com>
2020-10-31 15:27:00 +1000

    Update update-NodeRed.sh

--- update-NodeRed.sh
+++ update-NodeRed.sh
@@ -7,7 +7,7 @@ sudo cp /home/pi/MONICON/SESS_BS_ID002/flow.json /home/pi/.node-red/flows_SESS.j
 sudo cp /home/pi/MONICON/SESS_BS_ID002/flow_cred.json /home/pi/.node-red/flows_SESS_cred.json
 sudo cp /home/pi//MONICON/SESS_BS_ID002/package.json /home/pi/.node-red/package.json
 
-//sudo rm -r /home/pi/MONICON/SESS_BS_ID002
+sudo rm -r /home/pi/MONICON/SESS_BS_ID002
 
 echo Download complete
 
