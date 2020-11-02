e552db782cb6ab633fe3db4cf7c56e87c2fa7df4
jimmy232 <jamescurriehv@gmail.com>
2020-11-02 13:27:05 +1000

    Add files via upload

---
+++ update-NodeRed-monicon.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Download SESS_BS_ID002 from github repository
+
+sudo git clone https://github.com/jimmy232/MoniconPLC.git /home/pi/MONICON/MoniconPLC
+
+sudo cp /home/pi/MONICON/MoniconPLC/flow.json /home/pi/.node-red/flows_SESS.json
+sudo cp /home/pi/MONICON/MoniconPLC/flow_cred.json /home/pi/.node-red/flows_SESS_cred.json
+sudo cp /home/pi//MONICON/MoniconPLC/package.json /home/pi/.node-red/package.json
+
+sudo rm -r /home/pi/MONICON/MoniconPLC
+
+echo Download complete
+
+sudo reboot
