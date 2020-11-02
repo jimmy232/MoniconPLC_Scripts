87d3816f34c6fd43666162c53d834f1c1c4a4845
jimmy232 <jamescurriehv@gmail.com>
2020-11-02 13:46:42 +1000

    Update update-NodeRed-monicon.sh

--- update-NodeRed-monicon.sh
+++ update-NodeRed-monicon.sh
@@ -3,8 +3,8 @@
 
 sudo git clone https://github.com/jimmy232/MoniconPLC.git /home/pi/MONICON/MoniconPLC
 
-sudo cp /home/pi/MONICON/MoniconPLC/flow.json /home/pi/.node-red/flows_SESS.json
-sudo cp /home/pi/MONICON/MoniconPLC/flow_cred.json /home/pi/.node-red/flows_SESS_cred.json
+sudo cp /home/pi/MONICON/MoniconPLC/flow.json /home/pi/.node-red/flows_MONICON.json
+sudo cp /home/pi/MONICON/MoniconPLC/flow_cred.json /home/pi/.node-red/flows_MONICON_cred.json
 sudo cp /home/pi//MONICON/MoniconPLC/package.json /home/pi/.node-red/package.json
 
 sudo rm -r /home/pi/MONICON/MoniconPLC
