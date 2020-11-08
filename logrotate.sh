feee706f7c41a83b61642a47e068fbf790860010
jimmy232 <jamescurriehv@gmail.com>
2020-11-08 23:15:14 +1000

    Update logrotate.sh

--- logrotate.sh
+++ logrotate.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+# logrotate Config File
 
+sudo git clone https://github.com/jimmy232/MoniconPLCFiles.git /home/pi/MONICON/MoniconPLCFiles
+
+sudo cp /home/pi/MONICON/MoniconPLCFiles/logrotate.conf /etc/logrotate.conf
+
+sudo rm -r /home/pi/MONICON/MoniconPLCFiles
+
+echo Download complete
+
+sudo reboot
