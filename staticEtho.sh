9f9989b35f2da6ac841edb53c40d0f88466fb598
jimmy232 <jamescurriehv@gmail.com>
2020-12-09 16:20:42 +1000

    Update staticEtho.sh

--- staticEtho.sh
+++ staticEtho.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+# Assign Static Ethernet IP
 
+sudo git clone https://github.com/jimmy232/MoniconPLCFiles.git /home/pi/MONICON/MoniconPLCFiles
+
+sudo cp /home/pi/MONICON/MoniconPLCFiles/dhcpcd.conf /etc/dhcpcd.conf
+
+sudo rm -r /home/pi/MONICON/MoniconPLCFiles
+
+echo Download complete
+
+sudo reboot
