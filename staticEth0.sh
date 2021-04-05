35fb41ee879497cd08cb05466ebcdc330a80bf78
jimmy232 <jamescurriehv@gmail.com>
2021-04-05 11:31:48 +1000

    Update staticEth0.sh

--- staticEth0.sh
+++ staticEth0.sh
@@ -7,6 +7,11 @@ sudo cp /home/pi/MONICON/MoniconPLCFiles/dhcpcd.conf /etc/dhcpcd.conf
 
 sudo rm -r /home/pi/MONICON/MoniconPLCFiles
 
+# Allows User access to pors lower than 1024 "Especially for MODBUS 502"
+sudo setcap 'cap_net_bind_service=+ep' /usr/bin/node
+sudo setcap -v 'cap_net_bind_service=+ep' /usr/local/bin/node
+sudo setcap 'cap_net_bind_service=+ep' $(eval readlink -f `/usr/bin/node`)
+
 echo Download complete
 
 sudo reboot
