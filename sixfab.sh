3d4391cc18bce62eba994538bc98f5ce3c13a630
jimmy232 <jamescurriehv@gmail.com>
2021-05-10 21:49:58 +1000

    Create sixfab.sh

---
+++ sixfab.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Install SIXFAB python3 script to enable 3rd party status scripting
+
+sudo mkdir /home/pi/sixfab
+cd /home/pi/sixfab
+sudo git clone https://github.com/sixfab/Sixfab_RPi_CellularIoT_Library.git
+cd Sixfab_RPi_CellularIoT_Library
+sudo python3 setup.py install
+
+#sudo git clone https://github.com/jimmy232/MoniconPLCFiles.git /home/pi/MONICON/MoniconPLCFiles
+
+#sudo cp /home/pi/MONICON/MoniconPLCFiles/rc.local /etc/rc.local
+
+#sudo rm -r /home/pi/MONICON/MoniconPLCFiles
+
+echo Download complete
+
+sudo reboot
