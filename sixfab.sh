e098366f2f593cc6c7a8cd3dccc960fca10ea897
jimmy232 <jamescurriehv@gmail.com>
2021-05-23 15:47:53 +1000

    Update sixfab.sh

--- sixfab.sh
+++ sixfab.sh
@@ -6,21 +6,14 @@ then
     sudo mkdir /home/pi/sixfab
 fi
 
-
-sudo git clone https://github.com/jimmy232/MoniconPLCFiles.git /home/pi/MONICON/MoniconPLCFiles
-sudo cp /home/pi/MONICON/MoniconPLCFiles/sixfab.py /home/pi/sixfab/Sixfab_RPi_CellularIoT_Library
-sudo rm -r /home/pi/MONICON/MoniconPLCFiles
-
 cd /home/pi/sixfab
 sudo git clone https://github.com/sixfab/Sixfab_RPi_CellularIoT_Library.git
 cd Sixfab_RPi_CellularIoT_Library
 sudo python3 setup.py install
 
-#sudo git clone https://github.com/jimmy232/MoniconPLCFiles.git /home/pi/MONICON/MoniconPLCFiles
-
-#sudo cp /home/pi/MONICON/MoniconPLCFiles/rc.local /etc/rc.local
-
-#sudo rm -r /home/pi/MONICON/MoniconPLCFiles
+sudo git clone https://github.com/jimmy232/MoniconPLCFiles.git /home/pi/MONICON/MoniconPLCFiles
+sudo cp /home/pi/MONICON/MoniconPLCFiles/sixfab.py /home/pi/sixfab/Sixfab_RPi_CellularIoT_Library
+sudo rm -r /home/pi/MONICON/MoniconPLCFiles
 
 echo Download complete
 
