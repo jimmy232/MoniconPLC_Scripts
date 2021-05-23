b32fa6dc50033390e07a4185ac4a29cdf570551f
jimmy232 <jamescurriehv@gmail.com>
2021-05-23 15:31:26 +1000

    Update sixfab.sh

--- sixfab.sh
+++ sixfab.sh
@@ -2,6 +2,11 @@
 # Install SIXFAB python3 script to enable 3rd party status scripting
 
 sudo mkdir /home/pi/sixfab
+
+sudo git clone https://github.com/jimmy232/MoniconPLCFiles.git /home/pi/MONICON/MoniconPLCFiles
+sudo cp /home/pi/MONICON/MoniconPLCFiles/sixfab.py /home/pi/sixfab/Sixfab_RPi_CellularIoT_Library
+sudo rm -r /home/pi/MONICON/MoniconPLCFiles
+
 cd /home/pi/sixfab
 sudo git clone https://github.com/sixfab/Sixfab_RPi_CellularIoT_Library.git
 cd Sixfab_RPi_CellularIoT_Library
