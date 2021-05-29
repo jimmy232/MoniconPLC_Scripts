a98bfda4a5fcc13d0a650a6f43de28ba911b6ff2
jimmy232 <jamescurriehv@gmail.com>
2021-05-29 10:47:28 +1000

    Update staticEth0.sh

--- staticEth0.sh
+++ staticEth0.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # Assign Static Ethernet IP
 
+echo "installing MODBUS node red"
+# Install Node Red Modbus Library
+sudo npm install node-red-contrib-modbus
+
 echo "downloading repo..."
 sudo git clone https://github.com/jimmy232/MoniconPLCFiles.git /home/pi/MONICON/MoniconPLCFiles
 
@@ -13,13 +17,9 @@ sudo rm -r /home/pi/MONICON/MoniconPLCFiles
 echo "configuring user access for MODBUS 502"
 # Allows User access to pors lower than 1024 "Especially for MODBUS 502"
 sudo setcap 'cap_net_bind_service=+ep' /usr/bin/node
-#sudo setcap -v 'cap_net_bind_service=+ep' /usr/local/bin/node
-sudo setcap 'cap_net_bind_service=+ep' $(eval readlink -f `/usr/bin/node`)
+sudo setcap -v 'cap_net_bind_service=+ep' /usr/local/bin/node
+#sudo setcap 'cap_net_bind_service=+ep' $(eval readlink -f `/usr/bin/node`)
 
-echo "installing MODBUS node red"
-# Install Node Red Modbus Library
-sudo npm install node-red-contrib-modbus
-
-echo Download complete
+echo Scripting Complete
 
 sudo reboot
