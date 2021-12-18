febed3604c5f6a84a1a9ae7c1d447b5c0cf23240
jimmy232 <jamescurriehv@gmail.com>
2021-12-18 17:04:31 +1000

    Update and rename updateNodeRed002.sh to updateNodeRed003.sh

---
+++ updateNodeRed003.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Download SESS_BS_ID003 from github repository
+
+sudo git clone https://github.com/jimmy232/SESS_BS_ID002.git /home/pi/MONICON/SESS_BS_ID003
+
+sudo cp /home/pi/MONICON/SESS_BS_ID003/flow.json /home/pi/.node-red/flows_SESS.json
+sudo cp /home/pi/MONICON/SESS_BS_ID003/flow_cred.json /home/pi/.node-red/flows_SESS_cred.json
+sudo cp /home/pi//MONICON/SESS_BS_ID003/package.json /home/pi/.node-red/package.json
+
+sudo rm -r /home/pi/MONICON/SESS_BS_ID003
+
+echo Download complete
+
+sudo reboot
