3df4e9df2888fa652c5a9d1333e36c81070eb05f
jimmy232 <jamescurriehv@gmail.com>
2021-12-18 17:04:44 +1000

    Update updateNodeRed003.sh

--- updateNodeRed003.sh
+++ updateNodeRed003.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Download SESS_BS_ID003 from github repository
 
-sudo git clone https://github.com/jimmy232/SESS_BS_ID002.git /home/pi/MONICON/SESS_BS_ID003
+sudo git clone https://github.com/jimmy232/SESS_BS_ID003.git /home/pi/MONICON/SESS_BS_ID003
 
 sudo cp /home/pi/MONICON/SESS_BS_ID003/flow.json /home/pi/.node-red/flows_SESS.json
 sudo cp /home/pi/MONICON/SESS_BS_ID003/flow_cred.json /home/pi/.node-red/flows_SESS_cred.json
