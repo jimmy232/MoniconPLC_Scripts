6a47732968f929ee41c9c6aea92f3403588f6b54
jimmy232 <jamescurriehv@gmail.com>
2022-12-11 22:13:55 +1000

    Update updateMaterMackay.sh

--- updateMaterMackay.sh
+++ updateMaterMackay.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
+# Download MaterMackay from github repository
 
+sudo git clone https://github.com/jimmy232/MaterMackay.git /home/pi/MONICON/MaterMackay
+
+sudo cp /home/pi/MONICON/MaterMackay/flow.json /home/pi/.node-red/flows_SESS.json
+sudo cp /home/pi/MONICON/MaterMackay/flow_cred.json /home/pi/.node-red/flows_SESS_cred.json
+sudo cp /home/pi//MONICON/MaterMackay/package.json /home/pi/.node-red/package.json
+
+sudo rm -r /home/pi/MONICON/MaterMackay
+
+npm install
+
+echo Download complete
+
+sudo reboot
