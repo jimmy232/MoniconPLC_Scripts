7b05b3de476dbfaf536a0ec28c0087246ab858ad
jimmy232 <jamescurriehv@gmail.com>
2022-12-22 18:10:41 +1000

    Update Nodesettings.sh

--- Nodesettings.sh
+++ Nodesettings.sh
@@ -3,7 +3,7 @@
 
 sudo git clone https://github.com/jimmy232/MoniconPLCFiles.git /home/pi/MONICON/MoniconPLCFiles
 
-sudo cp /home/pi/MONICON/MoniconPLCFiles/settings.js .node-red/settings.js
+sudo cp /home/pi/MONICON/MoniconPLCFiles/settings.js /home/pi/.node-red/settings.js
 
 sudo rm -r /home/pi/MONICON/MoniconPLCFiles
 
