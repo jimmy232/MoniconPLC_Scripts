1324c2edfb929ff04f47916e68321f2b70cc57e4
jimmy232 <jamescurriehv@gmail.com>
2022-12-22 21:15:58 +1000

    Update updateMaterMBH.sh

--- updateMaterMBH.sh
+++ updateMaterMBH.sh
@@ -5,8 +5,8 @@ sudo npm cache clean --force
 
 sudo git clone https://github.com/jimmy232/MaterMBH.git /home/pi/MONICON/MaterMBH
 
-sudo cp /home/pi/MONICON/MaterMBH/flow.json /home/pi/.node-red/flows_SESS.json
-sudo cp /home/pi/MONICON/MaterMBH/flow_cred.json /home/pi/.node-red/flows_SESS_cred.json
+sudo cp /home/pi/MONICON/MaterMBH/flows.json /home/pi/.node-red/flows_SESS.json
+sudo cp /home/pi/MONICON/MaterMBH/flows_cred.json /home/pi/.node-red/flows_SESS_cred.json
 sudo cp /home/pi//MONICON/MaterMBH/package.json /home/pi/.node-red/package.json
 
 #sudo rm -r /home/pi/MONICON/MaterMBH
