9bed0281180dba598f39a09ef76d783dcab8cdfb
jimmy232 <jamescurriehv@gmail.com>
2022-12-22 20:57:04 +1000

    Update updateMaterMBH.sh

--- updateMaterMBH.sh
+++ updateMaterMBH.sh
@@ -7,10 +7,10 @@ sudo cp /home/pi/MONICON/MaterMBH/flow.json /home/pi/.node-red/flows_SESS.json
 sudo cp /home/pi/MONICON/MaterMBH/flow_cred.json /home/pi/.node-red/flows_SESS_cred.json
 sudo cp /home/pi//MONICON/MaterMBH/package.json /home/pi/.node-red/package.json
 
-sudo rm -r /home/pi/MONICON/MaterMBH
+#sudo rm -r /home/pi/MONICON/MaterMBH
 
 npm install
 
 echo Download complete
 
-sudo reboot
+#sudo reboot
